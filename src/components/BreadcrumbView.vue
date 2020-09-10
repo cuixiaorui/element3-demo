2332065acb2a7a9e08dc580215edd887c656ddb8
cuixiaorui <cui_xiaorui@126.com>
2020-09-10 18:24:20 +0800

    add demo for breadcrumb

--- src/components/BreadcrumbView.vue
+++ src/components/BreadcrumbView.vue
@@ -1,4 +1,11 @@
 <template>
+  <el-breadcrumb separator="/">
+    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+    <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+  </el-breadcrumb>
+
   <el-breadcrumb separator-class="el-icon-arrow-right">
     <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
     <el-breadcrumb-item>活动管理</el-breadcrumb-item>
