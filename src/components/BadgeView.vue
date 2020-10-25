1310a312bad886fa2f2bf8a9b5f411b3db0b528f
cuixiaorui <cui_xiaorui@126.com>
2020-10-25 22:55:50 +0800

    add badge demo

---
+++ src/components/BadgeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-badge :value="12" class="item">
+      <el-button size="small">评论</el-button>
+    </el-badge>
+    <el-badge :value="3" class="item">
+      <el-button size="small">回复</el-button>
+    </el-badge>
+    <el-badge :value="1" class="item" type="primary">
+      <el-button size="small">评论</el-button>
+    </el-badge>
+    <el-badge :value="2" class="item" type="warning">
+      <el-button size="small">回复</el-button>
+    </el-badge>
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.item {
+  margin-top: 10px;
+  margin-right: 40px;
+}
+</style>
