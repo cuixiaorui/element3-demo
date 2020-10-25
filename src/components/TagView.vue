94bb5f47fa1390288a6c09a361d59d2636ff02ca
cuixiaorui <cui_xiaorui@126.com>
2020-10-25 22:56:21 +0800

    add tag demo

---
+++ src/components/TagView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <el-tag>标签一</el-tag>
+    <el-tag type="success">标签二</el-tag>
+    <el-tag type="info">标签三</el-tag>
+    <el-tag type="warning">标签四</el-tag>
+    <el-tag type="danger">标签五</el-tag>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
