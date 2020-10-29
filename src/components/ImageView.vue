938c5308079d4b2fa05fb6e73aefafa437bf33a3
cuixiaorui <cui_xiaorui@126.com>
2020-10-29 23:42:04 +0800

    add image demo

---
+++ src/components/ImageView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="demo-image">
+    <div class="block" v-for="fit in fits" :key="fit">
+      <span class="demonstration">{{ fit }}</span>
+      <el-image
+        style="width: 100px; height: 100px"
+        :src="url"
+        :fit="fit"
+      ></el-image>
+    </div>
+  </div>
+</template>
+<script>
+import { reactive, toRefs } from "vue";
+export default {
+  setup() {
+    let state = reactive({
+      fits: ["fill", "contain", "cover", "none", "scale-down"],
+      url:
+        "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+    });
+    return { ...toRefs(state) };
+  },
+};
+</script>
