e5d42e579f6f16fed742a538974667ffb3b6b49e
cuixiaorui <cui_xiaorui@126.com>
2020-10-30 12:05:39 +0800

    add element3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import "element3/lib/theme-chalk/index.css";
 import { createApp } from "vue";
-import Element3 from "element3";
-// import Element3 from "../dist/element3-ui.esm-bundler";
+import { Element3 } from "./element3";
 import App from "./App.vue";
 
 const app = createApp(App);
